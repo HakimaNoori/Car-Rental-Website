2dfe42d03186506a5744defa63cd85426de8d836
HakimaNoori <hakima.noori.1379@gmail.com>
2025-01-31 21:32:34 +0430

    Add Navbar, Hero and About components.

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { useState } from "react";
+import Hero from "./components/Hero/Hero";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import About from "./components/About/About";
+
+const App = () => {
+
+  // Dark Mode Feature
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ?
+      localStorage.getItem("theme") : "dark"
+  );
+  const element = document.documentElement;
+  useEffect(() => {
+    if (theme === "dark") {
+      element.classList.add("dark");
+      localStorage.setItem(theme, "dark");
+    } else {
+      element.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [theme]);
+
+// AOS Initialization
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh()
+  }, []);
+  
+  return (
+    <div>
+      <Navbar theme={theme} setTheme={setTheme} />
+      <Hero theme={theme} />
+      <About/>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/About/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+    return (
+        <div className="about">
+            <h1>About Us</h1>
+            <p>Welcome to our car rental service. We offer a wide range of vehicles to suit your needs.</p>
+        </div>
+    );
+};
+
+export default About;
